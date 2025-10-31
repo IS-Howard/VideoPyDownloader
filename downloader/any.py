72507a9e2a5c36c2201909a2bf6e092be658520b
Howard <howard@intrising.com.tw>
2025-10-31 13:01:06 +0800

    feat: support m3u8 link

--- downloader/any.py
+++ downloader/any.py
@@ -9,14 +9,19 @@ def TryGetAnyVideo(link, TMP, downloadPath):
             os.makedirs(tmpPath)
         if not os.path.isdir(downloadPath):
             os.makedirs(downloadPath)
-        response = requests.get(link)
-        soup = bs(response.text, 'html.parser')
-        title_tag = soup.find('title')
-        title = title_tag.text if title_tag else None
-        if not title:
-            title = 'tmp_title'
-        title = FileNameClean(title)
-        chunks = Get_m3u8_chunklist(link)
+        chunks = []
+        title = 'tmp_title'
+        if link.endswith("index.m3u8"):
+            print("in")
+            res = requests.get(link, timeout=30, headers=global_headers)
+            chunks = Parse_m3u8(TMP, res, link)
+        else:
+            response = requests.get(link)
+            soup = bs(response.text, 'html.parser')
+            title_tag = soup.find('title')
+            title = title_tag.text if title_tag else 'tmp_title'
+            title = FileNameClean(title)
+            chunks = Get_m3u8_chunklist(link)
         print(title)
         Download_Chunks(chunks, TMP)
         #ffmpeg convert
