894a82c0027c917f7796a29c4a825c3fbab24501
Howard <howard@intrising.com.tw>
2025-11-19 23:49:58 +0800

    feat: ask for title for any source

--- downloader/any.py
+++ downloader/any.py
@@ -10,17 +10,11 @@ def TryGetAnyVideo(link, TMP, downloadPath):
         if not os.path.isdir(downloadPath):
             os.makedirs(downloadPath)
         chunks = []
-        title = 'tmp_title'
+        title = FileNameClean(input("檔名:"))
         if link.endswith("index.m3u8"):
-            print("in")
             res = requests.get(link, timeout=30, headers=global_headers)
             chunks = Parse_m3u8(TMP, res, link)
         else:
-            response = requests.get(link)
-            soup = bs(response.text, 'html.parser')
-            title_tag = soup.find('title')
-            title = title_tag.text if title_tag else 'tmp_title'
-            title = FileNameClean(title)
             chunks = Get_m3u8_chunklist(link)
         print(title)
         Download_Chunks(chunks, TMP)
